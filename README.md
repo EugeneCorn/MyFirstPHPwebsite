f45e32d3ad5b45ebd8633309717a433419f29c1f
Yevhen Korniievskyi <y.korniievskyi@dev-branch.com>
2023-05-21 22:49:40 +0300

    Another commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # My First PHP website
 
-This is my first PHP website I do using samples from this [website](https://www.codeproject.com/Articles/759094/Step-by-Step-PHP-Tutorials-for-Beginners-Creating).
+This is my first PHP website I do using samples from this Step-by-step [tutorial](https://www.codeproject.com/Articles/759094/Step-by-Step-PHP-Tutorials-for-Beginners-Creating).
+
+Feel free to do nothing here.
--- index.php
+++ index.php
@@ -8,8 +8,13 @@
         ?> </br> </br>
         <a href="login.php"> Click here to log in </a> <br/>
         <a href="register.php"> Click here to register </a> <br/>
-        <img src="http://conexwest2022-2.localhost/sites/default/files/styles/category_sm_max/public/2019-08/downloadfile-4%20%28Small%29_1.webp" href="Container" />
-        <img src="http://conexwest2022-2.localhost/sites/default/files/styles/category_sm_max/public/2019-08/downloadfile-4%20%28Small%29_1.png" href="Container" />
-        <img src="http://conexwest2022-2.localhost/sites/default/files/2019-08/downloadfile-4%20%28Small%29_1.png" href="Container_wm" />
+        <h1> Images from the local ConexWest website</h1> <br/>
+        <img src="http://conexwest2022-2.localhost/sites/default/files/styles/category_sm_max/public/2019-08/downloadfile-4%20%28Small%29_1.webp" href="Container" /> <br/>
+        <img src="http://conexwest2022-2.localhost/sites/default/files/styles/category_sm_max/public/2019-08/downloadfile-4%20%28Small%29_1.png" href="Container" /> <br/>
+        <img src="http://conexwest2022-2.localhost/sites/default/files/2019-08/downloadfile-4%20%28Small%29_1.png" href="Container_wm" /> <br/>
+        <h1> Images from the DevBranch Portainer ConexWest website </h1>
+        <img src="https://conexwest2022-2.devbranch.work/sites/default/files/2019-08/downloadfile-4%20%28Small%29_1.png" href="Conrtainer" /> <br />
+        <img src="https://DevBranch:DevBranch@conexwest2022-2.devbranch.work/sites/default/files/styles/category_sm_max/public/2019-08/downloadfile-4%20%28Small%29_1.png?itok=pNOHdp_4" href="Container" /> <br />
+        <img src="https://conexwest2022-2.devbranch.work/sites/default/files/styles/gallery_xs_max/public/2019-08/downloadfile-4%20%28Small%29_1.webp?itok=wA8QbsmT" href="Container" /> <br />
     </body>
 </html>
